ebd0de5188ea161a2325ff0d3e51f7a30b664f32
dima-webdev <nguyendmitry@gmail.com>
2019-10-10 17:12:20 +0300

    upload project

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const About = () => (
+  <div className="jumbotron">
+    <div className="container">
+      <h1 className="display-4">ReactApp</h1>
+      <p className="lead">
+        Version App 1.0.42
+      </p>
+    </div>
+  </div>
+)
